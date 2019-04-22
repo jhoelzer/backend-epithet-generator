d7f4c45e5d7ebacb2267e74ddc2d4d24eb3b5810
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2019-04-22 11:43:53 -0400

    helpers, test_helpers, other necessary files added

--- helpers.py
+++ helpers.py
@@ -3,21 +3,37 @@ import json
 
 
 class Vocabulary:
+    """
+    Handle loading in a JSON file with proper unfinished swears in it!
+
+    Usage:
+        data = Vocabulary.read_json("/path/to/data.json")
+    """
+
     def read_json(path, mode='r'):
-        # resources/data.json
         with open(path, mode=mode) as handle:
             return json.load(handle)
 
 
 class EpithetGenerator:
-    def generate_one(self):
-        data = Vocabulary.read_json("resources/data.json")
-        return "{} {} {}".format(random.choice(data["Column 1"]),
-                                  random.choice(data["Column 2"]),
-                                  random.choice(data["Column 3"]))
+    """
+    Handle creating and returning one or more epithets
 
-    def generate_multi(self, quantity):
+    Usage:
+        result = EpithetGenerator().one_random()
+        OR
+        result = EpithetGenerator().multi_random(quantity)
+    """
+
+    def one_random(self):
+        data = Vocabulary.read_json("resources/data.json")
+        first_word = random.choice(data["Column 1"])
+        second_word = random.choice(data["Column 2"])
+        third_word = random.choice(data["Column 3"])
+        return "{} {} {}!".format(first_word, second_word, third_word)
+
+    def multi_random(self, quantity):
         multi_epithets_list = []
         for _ in range(quantity):
-            multi_epithets_list.append(self.generate_one())
+            multi_epithets_list.append(self.one_random() + "!")
         return multi_epithets_list
